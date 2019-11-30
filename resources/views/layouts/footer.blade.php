9936414130e8d6193e3cf904cb3c8484e06a525f
ANITER Hichame <animer.hichame@gmail.com>
2019-11-30 20:10:08 +0100

    separate footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+@yield('content')
+<!-- Scripts
+    ================================================== -->
+    <script type="text/javascript" src="assets/plugins/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/viewportchecker.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/bootsnav.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/select2.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/wysihtml5-0.3.0.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/bootstrap-wysihtml5.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/datedropper.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/dropzone.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/loader.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/slick.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/gmap3.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/js/jquery.easy-autocomplete.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+</div>TESTBTEST
+</body>
+
+<!-- new-job-detail41:45-->
+</html>

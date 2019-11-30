335b3e3dc837f6c42bf503ea91409b6db3156895
ANITER Hichame <animer.hichame@gmail.com>
2019-11-30 19:03:44 +0100

    add create_resume Route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('create_resume', function(){
+    return view('candidate_create-resume');
+});
 Route::get('/', function () {
     return view('welcome');
 });

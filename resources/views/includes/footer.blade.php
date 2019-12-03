22d21a784ad1183823f3e2c85f4aca2444930400
Retina97 <animer.hichame@gmail.com>
2019-12-03 09:27:18 +0100

    fix connect button Bug

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,40 @@
+<div class="clearfix"></div>
+<div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="tab" role="tabpanel">
 
+                    <div class="tab-content" id="myModalLabel2">
+                        <div role="tabpanel" class="tab-pane fade in active" id="login">
+                            <img src="assets/img/logo.png" class="img-responsive" alt=""/>
+
+                            <div class="subscribe wow fadeInUp">
+                                <form class="form-inline" method="post">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control"
+                                                   placeholder="Adresse email" required=""><input type="password"
+                                                                                             name="password"
+                                                                                             class="form-control"
+                                                                                             placeholder="Mot de passe"
+                                                                                             required="">
+
+                                            <div class="center">
+                                                <button type="submit" id="login-btn" class="submit-btn"> Se Connecter
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Scripts
     ================================================== -->
     <script type="text/javascript" src="assets/plugins/js/jquery.min.js"></script>

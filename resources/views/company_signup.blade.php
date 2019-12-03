245695d4b16521c016c413ceb0fb1c36a1767dcf
Retina97 <animer.hichame@gmail.com>
2019-12-03 03:07:36 +0100

    update recruiter signup

--- resources/views/company_signup.blade.php
+++ resources/views/company_signup.blade.php
@@ -14,28 +14,28 @@
 						<div class="col-md-6 col-md-offset-3">
 							<div class="login-panel panel panel-default">
 								<div class="panel-heading">
-									<h3 class="panel-title">Create Company Account</h3>
+									<h3 class="panel-title">Inscription - Recruteur</h3>
 								</div>
 								<div class="panel-body">
 									<img src="assets/img/logo.png" class="img-responsive" />
 									<form role="form">
 										<fieldset>
 											<div class="form-group">
-												<input type="text" class="form-control" placeholder="Company Name" autofocus>
+												<input type="text" class="form-control" placeholder="Nom de compagnie" autofocus>
 											</div>
                       <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Your Email">
+                        <input type="email" class="form-control" placeholder="Email">
                       </div>
                       <div class="form-group">
-												<input type="password" class="form-control" placeholder="Password">
+												<input type="password" class="form-control" placeholder="Mot de passe">
 											</div>
                       <div class="form-group">
-												<input type="password" class="form-control" placeholder="Repeat your Password">
+												<input type="password" class="form-control" placeholder="Confirmer le mot de passe">
 											</div>
 											<!-- Change this to a button or input when using this as a form -->
-											<a href="" class="btn btn-login">Sign Up</a>
+											<a href="" class="btn btn-login">Inscription</a>
 										</fieldset>
-									</form>								
+									</form>
 								</div>
 							</div>
 						</div>

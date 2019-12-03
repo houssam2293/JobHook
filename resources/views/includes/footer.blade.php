5c4ea9716b8bba7af1393937babbf70fb79cb473
Retina97 <animer.hichame@gmail.com>
2019-12-03 09:28:25 +0100

    separate footer from acceuil

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,99 @@
+<!-- <div class="clearfix"></div>
+<footer class="footer">
+    <div class="row lg-menu">
+        <div class="container">
+            <div class="col-md-4 col-sm-4"><img src="assets/img/footer-logo.png" class="img-responsive" alt=""/>
+            </div>
+            <div class="col-md-8 co-sm-8 pull-right">
+                <ul>
+                    <li><a href="index-2.html" title="">Home</a></li>
+                    <li><a href="blog.html" title="">Blog</a></li>
+                    <li><a href="404.html" title="">404</a></li>
+                    <li><a href="faq.html" title="">FAQ</a></li>
+                    <li><a href="contact.html" title="">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row no-padding">
+        <div class="container">
+            <div class="col-md-3 col-sm-12">
+                <div class="footer-widget">
+                    <h3 class="widgettitle widget-title">About Job Stock</h3>
+
+                    <div class="textwidget">
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+
+                        <p>7860 North Park Place<br>San Francisco, CA 94120</p>
+
+                        <p><strong>Email:</strong> Support@careerdesk</p>
+
+                        <p><strong>Call:</strong> <a href="tel:+15555555555">555-555-1234</a></p>
+                        <ul class="footer-social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <div class="footer-widget">
+                    <h3 class="widgettitle widget-title">All Navigation</h3>
+
+                    <div class="textwidget">
+                        <div class="textwidget">
+                            <ul class="footer-navigation">
+                                <li><a href="manage-company.html" title="">Front-end Design</a></li>
+                                <li><a href="manage-company.html" title="">Android Developer</a></li>
+                                <li><a href="manage-company.html" title="">CMS Development</a></li>
+                                <li><a href="manage-company.html" title="">PHP Development</a></li>
+                                <li><a href="manage-company.html" title="">IOS Developer</a></li>
+                                <li><a href="manage-company.html" title="">Iphone Developer</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <div class="footer-widget">
+                    <h3 class="widgettitle widget-title">All Categories</h3>
+
+                    <div class="textwidget">
+                        <ul class="footer-navigation">
+                            <li><a href="manage-company.html" title="">Front-end Design</a></li>
+                            <li><a href="manage-company.html" title="">Android Developer</a></li>
+                            <li><a href="manage-company.html" title="">CMS Development</a></li>
+                            <li><a href="manage-company.html" title="">PHP Development</a></li>
+                            <li><a href="manage-company.html" title="">IOS Developer</a></li>
+                            <li><a href="manage-company.html" title="">Iphone Developer</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <div class="footer-widget">
+                    <h3 class="widgettitle widget-title">Connect Us</h3>
+
+                    <div class="textwidget">
+                        <form class="footer-form"><input type="text" class="form-control" placeholder="Your Name">
+                            <input type="text" class="form-control" placeholder="Email"><textarea
+                                    class="form-control" placeholder="Message"></textarea>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row copyright">
+        <div class="container">
+            <p><a target="_blank" href="https://www.templateshub.net">Templates Hub</a></p>
+        </div>
+    </div>
+</footer> -->
 <div class="clearfix"></div>
 <div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
     <div class="modal-dialog">

3cb7db1ce0e14b6bbe502f549585509c47673b6b
Retina97 <animer.hichame@gmail.com>
2019-12-03 03:02:50 +0100

    updating cadidate signup

--- resources/views/candidate_signup.blade.php
+++ resources/views/candidate_signup.blade.php
@@ -14,35 +14,35 @@
 						<div class="col-md-6 col-md-offset-3">
 							<div class="login-panel panel panel-default">
 								<div class="panel-heading">
-									<h3 class="panel-title">Create Candidate Account</h3>
+									<h3 class="panel-title">Inscription - Candidat</h3>
 								</div>
 								<div class="panel-body">
 									<img src="assets/img/logo.png" class="img-responsive" />
 									<form role="form">
 										<fieldset>
 											<div class="form-group">
-												<input type="text" class="form-control" placeholder="Your Name" autofocus>
+												<input type="text" class="form-control" placeholder="Nom" autofocus>
 											</div>
 											<div class="form-group">
-												<input type="text" class="form-control" placeholder="Last Name">
+												<input type="text" class="form-control" placeholder="Prenom">
 											</div>
                       <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Your Email">
+                        <input type="email" class="form-control" placeholder="Email">
                       </div>
                       <div class="form-group">
-												<input type="password" class="form-control" placeholder="Password">
+												<input type="password" class="form-control" placeholder="Mot de passe">
 											</div>
                       <div class="form-group">
-												<input type="password" class="form-control" placeholder="Repeat your Password">
+												<input type="password" class="form-control" placeholder="Confirmez le mot de passe">
 											</div>
 											<!-- Change this to a button or input when using this as a form -->
-											<a href="" class="btn btn-login">Sign Up</a>
+											<a href="" class="btn btn-login">Inscription</a>
 										</fieldset>
 									</form>
 									<ul class="social-login">
 										<li class="facebook-login"><a href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
-										<span>OR</span>
-										<li class="google-plus-login"><a href="#"><i class="fa fa-google-plus"></i>Facebook</a></li>
+										<span>OU</span>
+										<li class="google-plus-login"><a href="#"><i class="fa fa-google-plus"></i>Google</a></li>
 									</ul>
 								</div>
 							</div>

2d41abdd6904d213e93e146df87559a70a4b0500
Mohammed Bechlaghem <houssam2293@gmail.com>
2020-02-06 06:21:44 +0100

    fix route error with assets

--- resources/views/candidat/edit.blade.php
+++ resources/views/candidat/edit.blade.php
@@ -187,30 +187,30 @@
 			</section>
 			</form>
 
-			<script type="text/javascript" src="assets/plugins/js/jquery.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/viewportchecker.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/bootstrap.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/bootsnav.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/select2.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/wysihtml5-0.3.0.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/bootstrap-wysihtml5.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/datedropper.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/dropzone.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/loader.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/owl.carousel.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/slick.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/gmap3.min.js"></script>
-			<script type="text/javascript" src="assets/plugins/js/jquery.easy-autocomplete.min.js"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/jquery.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/viewportchecker.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/bootstrap.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/bootsnav.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/select2.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/wysihtml5-0.3.0.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/bootstrap-wysihtml5.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/datedropper.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/dropzone.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/loader.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/owl.carousel.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/slick.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/gmap3.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('assets/plugins/js/jquery.easy-autocomplete.min.js')}}"></script>
 			<!-- Date dropper js-->
 			<script src="#"></script>
 
 			<!-- Custom Js -->
-			<script src="assets/js/custom.js"></script>
+			<script src="{{asset('assets/js/custom.js')}}"></script>
 
 			<script>
 				$('#company-dob').dateDropper();
 			</script>
-			<script src="assets/js/jQuery.style.switcher.js"></script>
+			<script src="{{asset('assets/js/jQuery.style.switcher.js')}}"></script>
 			<script type="text/javascript">
 				$(document).ready(function() {
 					$('#styleOptions').styleSwitcher();
--- resources/views/recruteur/dashboard.blade.php
+++ resources/views/recruteur/dashboard.blade.php
@@ -29,7 +29,7 @@
   <!-- Portfolio Item 1 -->
   <div class="col-md-6 col-lg-4">
     <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-      <a href="{{ url('statistique') }}"><div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+      <a href="{{ url('/chart') }}"><div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
         <div class="portfolio-item-caption-content text-center text-white">
 
         </div>
